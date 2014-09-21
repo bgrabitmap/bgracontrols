e84b344605cda124e78a19a7312b7deb7bff3c9a
lainz <lainz@users.noreply.github.com>
2014-09-21 21:25:49 +0000

    added pix2hexagon in Pix2svg: now you can export to an hexagon map :)

--- test/test_extra/pix2svg/umain.pas
+++ test/test_extra/pix2svg/umain.pas
@@ -10,6 +10,8 @@ uses
 
 type
 
+  TPix2SVGStyle = (pix2rectangle, pix2ellipse, pix2hexagon);
+
   { TForm1 }
 
   TForm1 = class(TForm)
@@ -29,14 +31,40 @@ implementation
 
 {$R *.lfm}
 
-function Pix2svg(Bitmap: TBGRABitmap; mX, mY: single;
-  Ellipse, SkipTransparent: boolean): TStringList;
+function Pix2svg(Bitmap: TBGRABitmap; mX, mY: single; Style: TPix2SVGStyle;
+  SkipTransparent: boolean): TStringList;
 
   function BGRAtoRGBAstr(bgra: TBGRAPixel): string;
   begin
     Result := 'style="fill:rgb(' + IntToStr(bgra.red) + ',' +
       IntToStr(bgra.green) + ',' + IntToStr(bgra.blue) + '); fill-opacity:' +
       FloatToStr(bgra.alpha / 255) + ';';
+  end;
+
+  function hexagonStr(x, y: integer; p: PBGRAPixel): string;
+  var
+    p1, p2, p3, p4, p5, p6: string;
+  begin
+    if odd(y) then // impar
+    begin
+      p1 := FloatToStr(x + 1 / 2) + ' ' + FloatToStr(0.75 * y + 0.5 / 2) + ', ';
+      p2 := FloatToStr(x + 2 / 2) + ' ' + FloatToStr(0.75 * y + 1 / 2) + ', ';
+      p3 := FloatToStr(x + 2 / 2) + ' ' + FloatToStr(0.75 * y + 2 / 2) + ', ';
+      p4 := FloatToStr(x + 1 / 2) + ' ' + FloatToStr(0.75 * y + 2.5 / 2) + ', ';
+      p5 := FloatToStr(x + 0 / 2) + ' ' + FloatToStr(0.75 * y + 2 / 2) + ', ';
+      p6 := FloatToStr(x + 0 / 2) + ' ' + FloatToStr(0.75 * y + 1 / 2);
+    end
+    else // par
+    begin
+      p1 := FloatToStr(0.5 + x + 1 / 2) + ' ' + FloatToStr(0.75 * y + 0.5 / 2) + ', ';
+      p2 := FloatToStr(0.5 + x + 2 / 2) + ' ' + FloatToStr(0.75 * y + 1 / 2) + ', ';
+      p3 := FloatToStr(0.5 + x + 2 / 2) + ' ' + FloatToStr(0.75 * y + 2 / 2) + ', ';
+      p4 := FloatToStr(0.5 + x + 1 / 2) + ' ' + FloatToStr(0.75 * y + 2.5 / 2) + ', ';
+      p5 := FloatToStr(0.5 + x + 0 / 2) + ' ' + FloatToStr(0.75 * y + 2 / 2) + ', ';
+      p6 := FloatToStr(0.5 + x + 0 / 2) + ' ' + FloatToStr(0.75 * y + 1 / 2);
+    end;
+    Result := '    <polygon points="' + p1 + p2 + p3 + p4 + p5 +
+      p6 + '" ' + BGRAtoRGBAstr(p^) + '" />';
   end;
 
 var
@@ -59,14 +87,17 @@ begin
       if (SkipTransparent) and (p^.alpha = 0) then
       // nothing
       else
-      if Ellipse then
-        Result.Add('    <ellipse cx="' + FloatToStr(x * mX) +
-          '.5" cy="' + FloatToStr(y * mY) + '.5" rx="' + FloatToStr(0.5 * mX) +
-          '" ry="' + FloatToStr(0.5 * mY) + '" ' + BGRAtoRGBAstr(p^) + '"/>')
-      else
-        Result.Add('    <rect x="' + FloatToStr(x * mX) + '.5" y="' +
-          FloatToStr(y * mY) + '.5" width="' + FloatToStr(1 * mX) +
-          '" height="' + FloatToStr(1 * mY) + '" ' + BGRAtoRGBAstr(p^) + '"/>');
+        case Style of
+          pix2ellipse: Result.Add('    <ellipse cx="' + FloatToStr(x * mX) +
+              '.5" cy="' + FloatToStr(y * mY) + '.5" rx="' +
+              FloatToStr(0.5 * mX) + '" ry="' + FloatToStr(0.5 * mY) +
+              '" ' + BGRAtoRGBAstr(p^) + '"/>');
+          pix2rectangle: Result.Add('    <rect x="' + FloatToStr(x * mX) +
+              '.5" y="' + FloatToStr(y * mY) + '.5" width="' +
+              FloatToStr(1 * mX) + '" height="' + FloatToStr(1 * mY) +
+              '" ' + BGRAtoRGBAstr(p^) + '"/>');
+          pix2hexagon: Result.Add(hexagonStr(x, y, p));
+        end;
       Inc(p);
     end;
   end;
@@ -84,7 +115,7 @@ begin
   DecimalSeparator := '.';
 
   bitmap := TBGRABitmap.Create('lazpaint.png');
-  s := Pix2svg(bitmap, 1, 1, True, True);
+  s := Pix2svg(bitmap, 1, 1, pix2hexagon, True);
   s.SaveToFile('lazpaint.svg');
   s.Free;
 end;
