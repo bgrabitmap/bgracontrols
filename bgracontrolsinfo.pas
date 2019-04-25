b50a7a951111f336cc86b5c1a6e6ded5c824954b
lainz <leandrodiazoe@gmail.com>
2019-04-25 13:34:10 -0300

    BGRAControlsVersionStr

--- bgracontrolsinfo.pas
+++ bgracontrolsinfo.pas
@@ -10,7 +10,27 @@ uses
 const
   BGRAControlsVersion = 6010000;
 
+  function BGRAControlsVersionStr: string;
+
 implementation
+
+function BGRAControlsVersionStr: string;
+var numbers: TStringList;
+  i,remaining: cardinal;
+begin
+  numbers := TStringList.Create;
+  remaining := BGRAControlsVersion;
+  for i := 1 to 4 do
+  begin
+    numbers.Insert(0, IntToStr(remaining mod 100));
+    remaining := remaining div 100;
+  end;
+  while (numbers.Count > 1) and (numbers[numbers.Count-1]='0') do
+    numbers.Delete(numbers.Count-1);
+  numbers.Delimiter:= '.';
+  result := numbers.DelimitedText;
+  numbers.Free;
+end;
 
 end.
 
