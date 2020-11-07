eb907e97435292664a7db567060152a023db2ef8
lainz <leandrodiazoe@gmail.com>
2020-11-07 17:43:13 -0300

    Fix string limit

--- bgrasvgtheme.pas
+++ bgrasvgtheme.pas
@@ -14,39 +14,38 @@ type
 
   TBGRASVGTheme = class(TBGRATheme)
   private
-    FButtonActive: string;
-    FButtonHover: string;
-    FButtonNormal: string;
+    FButtonActive: TStringList;
+    FButtonHover: TStringList;
+    FButtonNormal: TStringList;
     FButtonSliceScalingBottom: integer;
     FButtonSliceScalingLeft: integer;
     FButtonSliceScalingRight: integer;
     FButtonSliceScalingTop: integer;
-    FCheckBoxChecked: string;
-    FCheckBoxUnchecked: string;
+    FCheckBoxChecked: TStringList;
+    FCheckBoxUnchecked: TStringList;
     FColorizeActive: TBGRAPixel;
     FColorizeDisabled: TBGRAPixel;
     FColorizeHover: TBGRAPixel;
     FColorizeNormal: TBGRAPixel;
-    FRadioButtonChecked: string;
-    FRadioButtonUnchecked: string;
-    procedure SetButtonActive(AValue: string);
-    procedure SetButtonHover(AValue: string);
-    procedure SetButtonNormal(AValue: string);
+    FRadioButtonChecked: TStringList;
+    FRadioButtonUnchecked: TStringList;
+    procedure SetButtonActive(AValue: TStringList);
+    procedure SetButtonHover(AValue: TStringList);
+    procedure SetButtonNormal(AValue: TStringList);
     procedure SetButtonSliceScalingBottom(AValue: integer);
     procedure SetButtonSliceScalingLeft(AValue: integer);
     procedure SetButtonSliceScalingRight(AValue: integer);
     procedure SetButtonSliceScalingTop(AValue: integer);
-    procedure SetCheckBoxChecked(AValue: string);
-    procedure SetCheckBoxUnchecked(AValue: string);
+    procedure SetCheckBoxChecked(AValue: TStringList);
+    procedure SetCheckBoxUnchecked(AValue: TStringList);
     procedure SetColorizeActive(AValue: TBGRAPixel);
     procedure SetColorizeDisabled(AValue: TBGRAPixel);
     procedure SetColorizeHover(AValue: TBGRAPixel);
     procedure SetColorizeNormal(AValue: TBGRAPixel);
-    procedure SetRadioButtonChecked(AValue: string);
-    procedure SetRadioButtonUnchecked(AValue: string);
-
+    procedure SetRadioButtonChecked(AValue: TStringList);
+    procedure SetRadioButtonUnchecked(AValue: TStringList);
   protected
-    procedure CheckEmptyResourceException(const aResource: String);
+    procedure CheckEmptyResourceException(const aResource: string);
     procedure SliceScalingDraw(const Source: TBGRASVG;
       const marginLeft, marginTop, marginRight, marginBottom: integer;
       const Dest: TBGRABitmap; DestDPI: integer);
@@ -69,16 +68,16 @@ type
     property ColorizeDisabled: TBGRAPixel read FColorizeDisabled
       write SetColorizeDisabled;
   published
-    property CheckBoxUnchecked: string read FCheckBoxUnchecked
+    property CheckBoxUnchecked: TStringList read FCheckBoxUnchecked
       write SetCheckBoxUnchecked;
-    property CheckBoxChecked: string read FCheckBoxChecked write SetCheckBoxChecked;
-    property RadioButtonUnchecked: string read FRadioButtonUnchecked
-      write SetRadioButtonUnchecked;
-    property RadioButtonChecked: string read FRadioButtonChecked
-      write SetRadioButtonChecked;
-    property ButtonNormal: string read FButtonNormal write SetButtonNormal;
-    property ButtonHover: string read FButtonHover write SetButtonHover;
-    property ButtonActive: string read FButtonActive write SetButtonActive;
+    property CheckBoxChecked: TStringList read FCheckBoxChecked write SetCheckBoxChecked;
+    property RadioButtonUnchecked: TStringList
+      read FRadioButtonUnchecked write SetRadioButtonUnchecked;
+    property RadioButtonChecked: TStringList
+      read FRadioButtonChecked write SetRadioButtonChecked;
+    property ButtonNormal: TStringList read FButtonNormal write SetButtonNormal;
+    property ButtonHover: TStringList read FButtonHover write SetButtonHover;
+    property ButtonActive: TStringList read FButtonActive write SetButtonActive;
     property ButtonSliceScalingLeft: integer
       read FButtonSliceScalingLeft write SetButtonSliceScalingLeft;
     property ButtonSliceScalingTop: integer
@@ -100,12 +99,13 @@ end;
 
 { TBGRASVGTheme }
 
-procedure TBGRASVGTheme.SetCheckBoxUnchecked(AValue: string);
+procedure TBGRASVGTheme.SetCheckBoxUnchecked(AValue: TStringList);
 begin
-  if FCheckBoxUnchecked = AValue then
-    Exit;
-  CheckEmptyResourceException(AValue);
-  FCheckBoxUnchecked := AValue;
+  CheckEmptyResourceException(AValue.Text);
+  if (AValue <> FCheckBoxUnchecked) then
+  begin
+    FCheckBoxUnchecked.Assign(AValue);
+  end;
 end;
 
 procedure TBGRASVGTheme.SetColorizeActive(AValue: TBGRAPixel);
@@ -136,23 +136,25 @@ begin
   FColorizeNormal := AValue;
 end;
 
-procedure TBGRASVGTheme.SetRadioButtonChecked(AValue: string);
+procedure TBGRASVGTheme.SetRadioButtonChecked(AValue: TStringList);
 begin
-  if FRadioButtonChecked = AValue then
-    Exit;
-  CheckEmptyResourceException(AValue);
-  FRadioButtonChecked := AValue;
+  CheckEmptyResourceException(AValue.Text);
+  if (AValue <> FRadioButtonChecked) then
+  begin
+    FRadioButtonChecked.Assign(AValue);
+  end;
 end;
 
-procedure TBGRASVGTheme.SetRadioButtonUnchecked(AValue: string);
+procedure TBGRASVGTheme.SetRadioButtonUnchecked(AValue: TStringList);
 begin
-  if FRadioButtonUnchecked = AValue then
-    Exit;
-  CheckEmptyResourceException(AValue);
-  FRadioButtonUnchecked := AValue;
+  CheckEmptyResourceException(AValue.Text);
+  if (AValue <> FRadioButtonUnchecked) then
+  begin
+    FRadioButtonUnchecked.Assign(AValue);
+  end;
 end;
 
-procedure TBGRASVGTheme.CheckEmptyResourceException(const aResource: String);
+procedure TBGRASVGTheme.CheckEmptyResourceException(const aResource: string);
 begin
   if (aResource.IsEmpty) then
     raise Exception.Create('Resource must not be empty.');
@@ -235,19 +237,26 @@ constructor TBGRASVGTheme.Create(AOwner: TComponent);
 begin
   inherited Create(AOwner);
   // https://material.io/resources/icons/
-  FCheckBoxUnchecked :=
+  FCheckBoxUnchecked := TStringList.Create;
+  FCheckBoxUnchecked.Text :=
     '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"/></svg>';
-  FCheckBoxChecked :=
+  FCheckBoxChecked := TStringList.Create;
+  FCheckBoxChecked.Text :=
     '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9 14l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/></svg>';
-  FRadioButtonUnchecked :=
+  FRadioButtonUnchecked := TStringList.Create;
+  FRadioButtonUnchecked.Text :=
     '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/></svg>';
-  FRadioButtonChecked :=
+  FRadioButtonChecked := TStringList.Create;
+  FRadioButtonChecked.Text :=
     '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 7c-2.76 0-5 2.24-5 5s2.24 5 5 5 5-2.24 5-5-2.24-5-5-5zm0-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/></svg>';
   // Button
-  FButtonNormal :=
-    '<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg   xmlns:dc="http://purl.org/dc/elements/1.1/"   xmlns:cc="http://creativecommons.org/ns#"   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"   xmlns:svg="http://www.w3.org/2000/svg"   xmlns="http://www.w3.org/2000/svg"   xmlns:xlink="http://www.w3.org/1999/xlink"   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"   width="32"   height="32"   viewBox="0 0 8.4666665 8.4666669"   version="1.1"   id="svg8"   inkscape:version="1.0.1 (3bc2e813f5, 2020-09-07)"   sodipodi:docname="button.svg">  <style     id="style833"></style>  <defs     id="defs2">    <linearGradient       inkscape:collect="always"       id="linearGradient858">      <stop         style="stop-color:#87cdde;stop-opacity:1"         offset="0"         id="stop854" />      <stop         style="stop-color:#ffffff;stop-opacity:1"         offset="1"         id="stop856" />    </linearGradient>    <linearGradient       inkscape:collect="always"       xlink:href="#linearGradient858"       id="linearGradient860"       x1="4.2333326"       y1="5.4568157"       x2="4.2333336"       y2="2.6458333"       gradientUnits="userSpaceOnUse" />  </defs>  <sodipodi:namedview     id="base"     pagecolor="#ffffff"     bordercolor="#666666"     borderopacity="1.0"     inkscape:pageopacity="0.0"     inkscape:pageshadow="2"     inkscape:zoom="1"     inkscape:cx="-159.25918"     inkscape:cy="-122.37605"     inkscape:document-units="px"     inkscape:current-layer="layer1"     inkscape:document-rotation="0"     showgrid="true"     units="px"     inkscape:window-width="1920"     inkscape:window-height="1017"     inkscape:window-x="-8"     inkscape:window-y="-8"     inkscape:window-maximized="1">    <inkscape:grid       type="xygrid"       id="grid837" />  </sodipodi:namedview>  <metadata     id="metadata5">    <rdf:RDF>      <cc:Work         rdf:about="">        <dc:format>image/svg+xml</dc:format>        <dc:type           rdf:resource="http://purl.org/dc/dcmitype/StillImage" />        <dc:title />      </cc:Work>    </rdf:RDF>  </metadata>  <g     inkscape:label="Capa 1"     inkscape:groupmode="layer"     id="layer1">    <path       vectorEffect="non-scaling-stroke"       id="rect835"       style="fill:#000000;stroke:#002255;stroke-width:1;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"       d="m 2.7400001,0.50000001 h 2.9866666 c 1.2409599,0 2.24,0.99903999 2.24,2.24000009 v 2.9866668 c 0,1.2409598 -0.9990401,2.24 -2.24,2.24 H 2.7400001 C 1.4990401,7.9666669 0.5,6.9676267 0.5,5.7266669 V 2.7400001 C 0.5,1.49904 1.4990401,0.50000001 2.7400001,0.50000001 Z" />    <path       vectorEffect="non-scaling-stroke"       id="rect835-8"       style="fill:url(#linearGradient860);fill-opacity:1;stroke:none;stroke-width:0.999996;stroke-linecap:butt;stroke-miterlimit:4;stroke-dasharray:none;paint-order:markers stroke fill"       d="m 2.6458334,0.52916666 h 3.175 c 1.1726329,0 2.1166662,0.94403314 2.1166662,2.11666644 v 3.1750002 c 0,1.1726334 -0.9440333,2.1166666 -2.1166662,2.1166666 h -3.175 c -1.1726332,0 -2.11666628,-0.9440332 -2.11666628,-2.1166666 V 2.6458331 c 0,-1.1726333 0.94403308,-2.11666644 2.11666628,-2.11666644 z" />  </g></svg>';
-  FButtonHover := FButtonNormal;
-  FButtonActive := FButtonNormal;
+  FButtonNormal := TStringList.Create;
+  FButtonNormal.Text :=
+    '<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg   xmlns:dc="http://purl.org/dc/elements/1.1/"   xmlns:cc="http://creativecommons.org/ns#"   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"   xmlns:svg="http://www.w3.org/2000/svg"   xmlns="http://www.w3.org/2000/svg"   xmlns:xlink="http://www.w3.org/1999/xlink"   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"   width="32"   height="32"   viewBox="0 0 32 32"   version="1.1"   id="svg8"   inkscape:version="1.0.1 (3bc2e813f5, 2020-09-07)"   sodipodi:docname="lime.svg">  <style     id="style833"></style>  <defs     id="defs2">    <linearGradient       inkscape:collect="always"       id="linearGradient858">      <stop         style="stop-color:#87cdde;stop-opacity:1"         offset="0"         id="stop854" />      <stop         style="stop-color:#ffffff;stop-opacity:1"         offset="1"         id="stop856" />    </linearGradient>    <linearGradient       inkscape:collect="always"       xlink:href="#linearGradient858"       id="linearGradient1415"       x1="3.9924731"       y1="5.9193549"       x2="3.9924731"       y2="2.788172"       gradientUnits="userSpaceOnUse"       gradientTransform="matrix(4.1517857,0,0,4.1517856,-1.5758928,-1.5758928)" />  </defs>  <sodipodi:namedview     id="base"     pagecolor="#ffffff"     bordercolor="#666666"     borderopacity="1.0"     inkscape:pageopacity="0.0"     inkscape:pageshadow="2"     inkscape:zoom="11.313708"     inkscape:cx="4.3902273"     inkscape:cy="23.941929"     inkscape:document-units="px"     inkscape:current-layer="layer1"     inkscape:document-rotation="0"     showgrid="true"     units="px"     inkscape:window-width="1920"     inkscape:window-height="1017"     inkscape:window-x="-8"     inkscape:window-y="-8"     inkscape:window-maximized="1">    <inkscape:grid       type="xygrid"       id="grid837" />  </sodipodi:namedview>  <metadata     id="metadata5">    <rdf:RDF>      <cc:Work         rdf:about="">        <dc:format>image/svg+xml</dc:format>        <dc:type           rdf:resource="http://purl.org/dc/dcmitype/StillImage" />        <dc:title></dc:title>      </cc:Work>    </rdf:RDF>  </metadata>  <g     inkscape:label="Capa 1"     inkscape:groupmode="layer"     id="layer1">    <path       vectorEffect="non-scaling-stroke"       id="rect835"       style="fill:url(#linearGradient1415);fill-opacity:1;stroke:#002255;stroke-width:1;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"       d="M 9.8000004,0.50000004 H 22.2 c 5.1522,0 9.3,4.14779986 9.3,9.30000016 V 22.2 c 0,5.152199 -4.1478,9.3 -9.3,9.3 H 9.8000004 C 4.6478005,31.5 0.50000005,27.352199 0.50000005,22.2 V 9.8000002 c 0,-5.1522003 4.14780045,-9.30000016 9.30000035,-9.30000016 z" />  </g></svg>';
+  FButtonHover := TStringList.Create;
+  FButtonHover.Text := FButtonNormal.Text;
+  FButtonActive := TStringList.Create;
+  FButtonActive.Text := FButtonNormal.Text;
   FButtonSliceScalingLeft := 10;
   FButtonSliceScalingTop := 10;
   FButtonSliceScalingRight := 10;
@@ -261,6 +270,13 @@ end;
 
 destructor TBGRASVGTheme.Destroy;
 begin
+  FCheckBoxUnchecked.Free;
+  FCheckBoxChecked.Free;
+  FRadioButtonUnchecked.Free;
+  FRadioButtonChecked.Free;
+  FButtonNormal.Free;
+  FButtonHover.Free;
+  FButtonActive.Free;
   inherited Destroy;
 end;
 
@@ -274,10 +290,10 @@ begin
   with ASurface do
   begin
     case State of
-      btbsNormal: svg := TBGRASVG.CreateFromString(FButtonNormal);
-      btbsHover: svg := TBGRASVG.CreateFromString(FButtonHover);
-      btbsActive: svg := TBGRASVG.CreateFromString(FButtonActive);
-      btbsDisabled: svg := TBGRASVG.CreateFromString(FButtonNormal);
+      btbsNormal: svg := TBGRASVG.CreateFromString(FButtonNormal.Text);
+      btbsHover: svg := TBGRASVG.CreateFromString(FButtonHover.Text);
+      btbsActive: svg := TBGRASVG.CreateFromString(FButtonActive.Text);
+      btbsDisabled: svg := TBGRASVG.CreateFromString(FButtonNormal.Text);
     end;
     SliceScalingDraw(svg{%H-}, FButtonSliceScalingLeft, FButtonSliceScalingTop,
       FButtonSliceScalingRight, FButtonSliceScalingBottom, ASurface.Bitmap,
@@ -323,9 +339,9 @@ begin
   begin
     BitmapRect := RectWithSize(ARect.Left, ARect.Top, ARect.Height, ARect.Height);
     if Checked then
-      svg := TBGRASVG.CreateFromString(FRadioButtonChecked)
+      svg := TBGRASVG.CreateFromString(FRadioButtonChecked.Text)
     else
-      svg := TBGRASVG.CreateFromString(FRadioButtonUnchecked);
+      svg := TBGRASVG.CreateFromString(FRadioButtonUnchecked.Text);
     svg.StretchDraw(Bitmap.Canvas2D, 0, 0, Bitmap.Width, Bitmap.Height);
     svg.Free;
     case State of
@@ -351,36 +367,40 @@ begin
   end;
 end;
 
-procedure TBGRASVGTheme.SetCheckBoxChecked(AValue: string);
+procedure TBGRASVGTheme.SetCheckBoxChecked(AValue: TStringList);
 begin
-  if FCheckBoxChecked = AValue then
-    Exit;
-  CheckEmptyResourceException(AValue);
-  FCheckBoxChecked := AValue;
+  CheckEmptyResourceException(AValue.Text);
+  if (AValue <> FCheckBoxChecked) then
+  begin
+    FCheckBoxChecked.Assign(AValue);
+  end;
 end;
 
-procedure TBGRASVGTheme.SetButtonActive(AValue: string);
+procedure TBGRASVGTheme.SetButtonActive(AValue: TStringList);
 begin
-  if FButtonActive = AValue then
-    Exit;
-  CheckEmptyResourceException(AValue);
-  FButtonActive := AValue;
+  CheckEmptyResourceException(AValue.Text);
+  if (AValue <> FButtonActive) then
+  begin
+    FButtonActive.Assign(AValue);
+  end;
 end;
 
-procedure TBGRASVGTheme.SetButtonHover(AValue: string);
+procedure TBGRASVGTheme.SetButtonHover(AValue: TStringList);
 begin
-  if FButtonHover = AValue then
-    Exit;
-  CheckEmptyResourceException(AValue);
-  FButtonHover := AValue;
+  CheckEmptyResourceException(AValue.Text);
+  if (AValue <> FButtonHover) then
+  begin
+    FButtonHover.Assign(AValue);
+  end;
 end;
 
-procedure TBGRASVGTheme.SetButtonNormal(AValue: string);
+procedure TBGRASVGTheme.SetButtonNormal(AValue: TStringList);
 begin
-  if FButtonNormal = AValue then
-    Exit;
-  CheckEmptyResourceException(AValue);
-  FButtonNormal := AValue;
+  CheckEmptyResourceException(AValue.Text);
+  if (AValue <> FButtonNormal) then
+  begin
+    FButtonNormal.Assign(AValue);
+  end;
 end;
 
 procedure TBGRASVGTheme.SetButtonSliceScalingBottom(AValue: integer);
@@ -422,9 +442,9 @@ begin
   begin
     BitmapRect := RectWithSize(ARect.Left, ARect.Top, ARect.Height, ARect.Height);
     if Checked then
-      svg := TBGRASVG.CreateFromString(FCheckBoxChecked)
+      svg := TBGRASVG.CreateFromString(FCheckBoxChecked.Text)
     else
-      svg := TBGRASVG.CreateFromString(FCheckBoxUnchecked);
+      svg := TBGRASVG.CreateFromString(FCheckBoxUnchecked.Text);
     svg.StretchDraw(Bitmap.Canvas2D, 0, 0, Bitmap.Width, Bitmap.Height);
     svg.Free;
     case State of
