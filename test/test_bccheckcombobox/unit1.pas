5e2bb96deeafc79885f9c8f8bdde82b3279d0d32
Leandro Diaz <lainz@users.noreply.github.com>
2023-11-29 17:44:32 -0300

    bgracontrols v8

---
+++ test/test_bccheckcombobox/unit1.pas
@@ -0,0 +1,30 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, BCComboBox;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    BCComboBox1: TBCComboBox;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
